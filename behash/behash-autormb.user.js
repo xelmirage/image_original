de008c30bb06ca6bd6e376dd704f602e5917e1e1
xelmirage <xelmirage@users.noreply.github.com>
2021-03-20 19:24:11 +0800

    Update behash-autormb.user.js

--- behash/behash-autormb.user.js
+++ behash/behash-autormb.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       xelmirage
-// @match        http://behash.com/user
+// @match        http://*.behash.com/user
 // @grant        none
 // ==/UserScript==
 
